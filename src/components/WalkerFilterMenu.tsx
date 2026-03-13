b6c43ec378d1c7ce2b5ef207063b68db0e173165
juan obrach <juanobrach@gmail.com>
2026-03-13 11:28:58 -0300

    fix: Adjust WalkerFilterMenu's top offset.

--- src/components/WalkerFilterMenu.tsx
+++ src/components/WalkerFilterMenu.tsx
@@ -48,7 +48,7 @@ export function WalkerFilterMenu({
   return (
     <div
       className={cn(
-        'absolute top-6 left-0 right-0 z-50 px-4 md:px-8 transition-opacity duration-1000',
+        'absolute top-3 left-0 right-0 z-50 px-4 md:px-8 transition-opacity duration-1000',
         isIdle ? 'opacity-0' : 'opacity-100',
       )}
     >
